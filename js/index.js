2e874d7912dd24f6bcd533917853692b4ff17a2a
Kelly <kellylovelu@gmail.com>
2017-07-31 16:10:59 -0400

    update arrays and html

--- js/index.js
+++ js/index.js
@@ -1,31 +1,24 @@
+//Add event listener to letter buttons and call play function inside
 window.onload = function () {
 	var letters = document.getElementById("letters").children
 	for (letter of letters) {
     letter.addEventListener("click", function(e){ 
+    	e.target.disabled = 'true'
     	play(this.innerText)
-    	console.log(this.innerText)
+    	
     })
   }
 }
 
-function id(str){
-	return document.getElementById(str)
-}
-// var start = id('start')
-var phrase = id('phrase')
-// var letter = id('letter')
-
 //Array of words
-var words = [
-	'JAVASCRIPT',
-	'APPLESAUCE',
-	'ANIMAL',
-	'WATERMELON'
-]
+var words = ['BANANA','PHILADELPHIA','OSTRICH','JUPITER','CHEESEBURGER','CAPPUCCINO']
+
+//Array of categories
+var categories = ['FRUIT','CITY','ANIMAL','PLANET','FOOD','DRINK']
 
 //Get random word
 var word = words[Math.floor(Math.random() * words.length)]
-// var hint = hintArray[Math.floor(random * words.length)]
+var hint = categories[Math.floor(random * words.length)]
 
 //Answer array
 var answerArray = []
@@ -33,7 +26,10 @@ var answerArray = []
 for (let i = 0; i < word.length; i++){
 	answerArray[i] = "_"
 }
-phrase.innerHTML = answerArray.join(" ")
+
+var guessWord = document.getElementById('guessWord')
+var category = document.getElementById('category')
+guessWord.innerHTML = answerArray.join(" ")
 
 //Keep track of letters that remain to be guessed
 var remainingLetters = word.length
@@ -43,18 +39,18 @@ function play(letter){
 	// The Game Loop
 	if (remainingLetters > 0) {
 		
-		phrase.innerHTML = answerArray
+		guessWord.innerHTML = answerArray
 
 		var guess = letter
 		for (let j = 0; j < word.length; j++){
 			if (word[j] === guess){
 				answerArray[j] = guess
 				remainingLetters--
-				console.log(letter)
 			} 
 		}
 	}
-	phrase.innerHTML = answerArray.join(" ")
+	guessWord.innerHTML = answerArray.join(" ")
+	
 }
 
 
