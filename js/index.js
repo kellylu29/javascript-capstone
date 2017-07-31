a7b138e101355004c84bf306f2edd92a4f049100
Kelly <kellylovelu@gmail.com>
2017-07-31 13:52:10 -0400

    update html buttons and javascript

--- js/index.js
+++ js/index.js
@@ -4,62 +4,48 @@ function id(str){
 
 var start = id('start')
 var phrase = id('phrase')
-var letter = id('letter')
+// var letter = id('letter')
 
-//Choosing a random word
-//Create and array of words
+//Array of words
 var words = [
-	'JAVASCRIPT',
+	'JAVASCRIPTL',
 	'APPLESAUCE',
 	'ANIMAL',
 	'WATERMELON'
 ]
 
+//Get random word
 var word = words[Math.floor(Math.random() * words.length)]
+// var hint = hintArray[Math.floor(random * words.length)]
 
-//Set up answer array
+//Answer array
 var answerArray = []
 
-for (var i = 0; i < word.length; i++){
+for (let i = 0; i < word.length; i++){
 	answerArray[i] = "_"
 }
+phrase.innerHTML = answerArray.join(" ")
 
-
-
-//Create variable to keep track of letters that remain to be guessed
+//Keep track of letters that remain to be guessed
 var remainingLetters = word.length
 
-class Game {
-	constructor(){
-
+function play(letter){
+		
+	// The Game Loop
+	while (remainingLetters > 0) {
+		
+		phrase.innerHTML = answerArray.join(" ")
+	
+		var guess = letter
+		for (let j = 0; j < word.length; j++){
+			if (word[j] === guess){
+			answerArray[j] = guess
+			remainingLetters--
+			} 
+		}
 	}
-	static play(){
-		start.addEventListener('click',function(){
-			start.classList.add('hidden')
-			
-
-			letter.addEventListener('click',function(){
-			// The Game Loop
-				while (remainingLetters > 0) {
-					//Show the player their progress
-					phrase.innerHTML = answerArray.join(' ')
-					//Get a guess from the player
-					var guess = letter.innerHTML	
-					for (var j = 0; j < word.length; j++){
-						if (word[j] === guess){
-						answerArray[j] = guess
-						remainingLetters--
-						}
-					}
-				// The end of the Game Loop
-
-				}
-				
-			})
-			phrase.innerHTML = answerArray.join(" ")	
-		})	
-
-	}
+	phrase.innerHTML = answerArray.join(" ")	
 }
 
-Game.play()
+
+
